27718a2eaf3918c9f66f8401a600fcfa07f2db29
trtoledo <trtoledo1977@gmail.com>
2019-03-28 21:35:54 -0400

    All done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:index, :new, :create, :show, :update, :edit]
+  # patch 'posts/:id', to: 'posts#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
